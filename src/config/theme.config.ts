0f159ab81c85df1ed4a1848a3930d6832ce11862
chapterchats <chapterchatswithcaroline@yahoo.com>
2026-09-19 13:30:20 +0530

    Update theme.config.ts

--- src/config/theme.config.ts
+++ src/config/theme.config.ts
@@ -20,7 +20,7 @@ export const NAVIGATION = [
   { to: "/blog", label: "Blog" },
   { to: "/about", label: "About" },
   { to: "/contact", label: "Contact" },
-  { to: "/slug: book-reviews", name: "Book Reviews", label: "Book Reviews" },
+  { to: "/slug: book-reviews", label: "Book Reviews" },
 ];
 
 export const CONTACT = {
