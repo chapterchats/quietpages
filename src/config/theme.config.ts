57d20df9e51d5af22acd0a64607e19b832be8ec9
chapterchats <chapterchatswithcaroline@yahoo.com>
2026-09-19 12:19:39 +0530

    Update theme.config.ts

--- src/config/theme.config.ts
+++ src/config/theme.config.ts
@@ -16,6 +16,7 @@ export const SITE = {
 
 export const NAVIGATION = [
   { to: "/", label: "Home" },
+  { to: "/", label: "Book Reviews" },
   { to: "/blog", label: "Blog" },
   { to: "/about", label: "About" },
   { to: "/contact", label: "Contact" },
