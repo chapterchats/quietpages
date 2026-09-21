6f8544107265626130f4e78f67ce0da44649c3ba
chapterchats <chapterchatswithcaroline@yahoo.com>
2026-09-21 19:16:18 +0530

    Update theme.config.ts

--- src/config/theme.config.ts
+++ src/config/theme.config.ts
@@ -31,9 +31,8 @@ export const CONTACT = {
   goodreadsUrl: "https://www.goodreads.com/user/show/95870560-caroline-d-cruz",
 };
 
-<div set:html={`
-  <script async src="https://subscribe-forms.beehiiv.com/v3/loader.js" data-beehiiv-form="374a06e1-7390-4dfc-bf2d-355dacf473af"></script>
-`} />
+<script async={true} src="https://subscribe-forms.beehiiv.com/v3/loader.js" data-beehiiv-form="374a06e1-7390-4dfc-bf2d-355dacf473af"></script>
+
 
 export const FORMS = {
   contact: {
