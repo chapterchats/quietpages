3122e3e2bc156fe3b6a598de952c1707466907d9
chapterchats <chapterchatswithcaroline@yahoo.com>
2026-09-22 16:13:52 +0530

    Update theme.config.ts

--- src/config/theme.config.ts
+++ src/config/theme.config.ts
@@ -63,12 +63,12 @@ export const authors = [
     avatar: "/avatars/caroline-dcruz.svg",
   },
   {
-    slug: "caru",
+    slug: "carol-d",
     name: "Caroline Dcruz",
     bio: "An book reviewer, reviewing every book I read one chapter at a time",
     longBio:
       "Caroline reviews and recommendations books that she reads and gives you valuable insights on them",
-    avatar: "/avatars/caru.png",
+    avatar: "/avatars/carol-d.png",
   }
 ];
 
